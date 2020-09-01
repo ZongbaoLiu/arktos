58d197936548bd8735637e9f6fd869dabab78a1e
Wang Jun <guaklw@163.com>
2020-09-01 15:16:43 +0800

    add cloudgateway api defination

---
+++ pkg/apis/cloudgateway/v1/types.go
@@ -0,0 +1,23 @@
+package v1
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+// +genclient
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ESite describe the edge site resource definition
+type ESite struct {
+	metav1.TypeMeta
+	metav1.ObjectMeta
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Ojbect
+
+// list type
+type ESiteList struct {
+	metav1.TypeMeta
+	metav1.ListMeta
+
+	Items []ESite
+}
