2df2b20a5b4df3d8a851f046dfc21c7f4f33326e
Wang Jun <guaklw@163.com>
2020-09-01 19:01:41 +0800

    add egateway definition

--- pkg/apis/cloudgateway/v1/register.go
+++ pkg/apis/cloudgateway/v1/register.go
@@ -30,6 +30,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		SchemeGroupVersion,
 		&ESite{},
 		&ESiteList{},
+		&EGateway{},
+		&EGatewayList{},
 		)
 
 	// register the type in the scheme
--- pkg/apis/cloudgateway/v1/types.go
+++ pkg/apis/cloudgateway/v1/types.go
@@ -21,3 +21,32 @@ type ESiteList struct {
 
 	Items []ESite		`json:"items"`
 }
+
+// +genclient
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// EGateway describe the edge gateway definition
+type EGateway struct {
+	metav1.TypeMeta		`json:",inline"`
+	metav1.ObjectMeta	`json:"metadata,omitempty"`
+
+	// Ip address of the gateway
+	Ip string
+
+	// Virtual presence ip address cidr of this gateway
+	VirtualPresenceIPcidr string
+
+	// ESiteName associated to the gateway
+	ESiteName string
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// list type
+type EGatewayList struct {
+	metav1.TypeMeta		`json:",inline"`
+	metav1.ListMeta		`json:"metadata"`
+
+	Items []EGateway	`json:"items"`
+}
