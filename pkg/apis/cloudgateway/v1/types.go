833e55fc4d813242497d16293dd1d5382f04b2a4
ZongbaoLiu <liu9219@yeah.net>
2020-09-02 10:51:16 +0800

    add cloudhub module and api

--- pkg/apis/cloudgateway/v1/types.go
+++ pkg/apis/cloudgateway/v1/types.go
@@ -1,6 +1,9 @@
 package v1
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"time"
+)
 
 // +genclient
 // +genclient:noStatus
@@ -49,4 +52,112 @@ type EGatewayList struct {
 	metav1.ListMeta		`json:"metadata"`
 
 	Items []EGateway	`json:"items"`
+}
+
+// KubeAPIConfig indicates the configuration for interacting with k8s server
+type KubeAPIConfig struct {
+	// Master indicates the address of the Kubernetes API server (overrides any value in KubeConfig)
+	// such as https://127.0.0.1:8443
+	// default ""
+	// Note: Can not use "omitempty" option,  It will affect the output of the default configuration file
+	Master string `json:"master"`
+	// ContentType indicates the ContentType of message transmission when interacting with k8s
+	// default application/vnd.kubernetes.protobuf
+	ContentType string `json:"contentType,omitempty"`
+	// QPS to while talking with kubernetes apiserve
+	// default 100
+	QPS int32 `json:"qps,omitempty"`
+	// Burst to use while talking with kubernetes apiserver
+	// default 200
+	Burst int32 `json:"burst,omitempty"`
+	// KubeConfig indicates the path to kubeConfig file with authorization and master location information.
+	// default "/root/.kube/config"
+	// +Required
+	KubeConfig string `json:"kubeConfig"`
+}
+
+// CloudHub indicates the config of CloudHub module.
+// CloudHub is a web socket or quic server responsible for watching changes at the cloud side,
+// caching and sending messages to EdgeHub.
+type CloudHub struct {
+	// Enable indicates whether CloudHub is enabled, if set to false (for debugging etc.),
+	// skip checking other CloudHub configs.
+	// default true
+	Enable bool `json:"enable,omitempty"`
+	// KeepaliveInterval indicates keep-alive interval (second)
+	// default 30
+	KeepaliveInterval int32 `json:"keepaliveInterval,omitempty"`
+	// NodeLimit indicates node limit
+	// default 1000
+	NodeLimit int32 `json:"nodeLimit,omitempty"`
+	// TLSCAFile indicates ca file path
+	// default "/etc/kubeedge/ca/rootCA.crt"
+	TLSCAFile string `json:"tlsCAFile,omitempty"`
+	// TLSCAKeyFile indicates caKey file path
+	// default "/etc/kubeedge/ca/rootCA.key"
+	TLSCAKeyFile string `json:"tlsCAKeyFile,omitempty"`
+	// TLSPrivateKeyFile indicates key file path
+	// default "/etc/kubeedge/certs/server.crt"
+	TLSCertFile string `json:"tlsCertFile,omitempty"`
+	// TLSPrivateKeyFile indicates key file path
+	// default "/etc/kubeedge/certs/server.key"
+	TLSPrivateKeyFile string `json:"tlsPrivateKeyFile,omitempty"`
+	// WriteTimeout indicates write time (second)
+	// default 30
+	WriteTimeout int32 `json:"writeTimeout,omitempty"`
+	// Quic indicates quic server info
+	Quic *CloudHubQUIC `json:"quic,omitempty"`
+	// WebSocket indicates websocket server info
+	// +Required
+	WebSocket *CloudHubWebSocket `json:"websocket,omitempty"`
+	// HTTPS indicates https server info
+	// +Required
+	HTTPS *CloudHubHTTPS `json:"https,omitempty"`
+	// AdvertiseAddress sets the IP address for the cloudcore to advertise.
+	AdvertiseAddress []string `json:"advertiseAddress,omitempty"`
+	// EdgeCertSigningDuration indicates the validity period of edge certificate
+	// default 365d
+	EdgeCertSigningDuration time.Duration `json:"edgeCertSigningDuration,omitempty"`
+}
+
+// CloudHubQUIC indicates the quic server config
+type CloudHubQUIC struct {
+	// Enable indicates whether enable quic protocol
+	// default false
+	Enable bool `json:"enable,omitempty"`
+	// Address set server ip address
+	// default 0.0.0.0
+	Address string `json:"address,omitempty"`
+	// Port set open port for quic server
+	// default 10001
+	Port uint32 `json:"port,omitempty"`
+	// MaxIncomingStreams set the max incoming stream for quic server
+	// default 10000
+	MaxIncomingStreams int32 `json:"maxIncomingStreams,omitempty"`
+}
+
+// CloudHubWebSocket indicates the websocket config of CloudHub
+type CloudHubWebSocket struct {
+	// Enable indicates whether enable websocket protocol
+	// default true
+	Enable bool `json:"enable,omitempty"`
+	// Address indicates server ip address
+	// default 0.0.0.0
+	Address string `json:"address,omitempty"`
+	// Port indicates the open port for websocket server
+	// default 10000
+	Port uint32 `json:"port,omitempty"`
+}
+
+// CloudHubHttps indicates the http config of CloudHub
+type CloudHubHTTPS struct {
+	// Enable indicates whether enable Https protocol
+	// default true
+	Enable bool `json:"enable,omitempty"`
+	// Address indicates server ip address
+	// default 0.0.0.0
+	Address string `json:"address,omitempty"`
+	// Port indicates the open port for HTTPS server
+	// default 10002
+	Port uint32 `json:"port,omitempty"`
 }
---
+++ pkg/cloudgateway/common/modules/group.go
@@ -0,0 +1,5 @@
+package modules
+
+const (
+	HubGroup = "cloudhub"
+)
